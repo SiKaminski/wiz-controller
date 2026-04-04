a9fcb70e3e3d59ca1fc2eb4e37316676e418d3a5
SiKaminski <simonkaminski1100@gmail.com>
2026-04-03 22:36:40 -0700

    Broadcast bulb ips

--- src/source.cpp
+++ src/source.cpp
@@ -1,11 +1,55 @@
 #include "bulb.hpp"
 
+// #include <asm-generic/socket.h>
+#include <iostream>
+#include <cstring>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #include <string>
 #include <chrono>
 #include <thread>
 #include <stdio.h>
 
 constexpr int BULB_COUNT {1};
+
+// int main()
+// {
+//     int sock = socket(AF_INET, SOCK_DGRAM, 0);
+// 
+//     int broadcastEnable = 1;
+//     setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
+// 
+//     sockaddr_in addr{};
+//     addr.sin_family = AF_INET;
+//     addr.sin_port = htons(38899);
+//     addr.sin_addr.s_addr = inet_addr("255.255.255.255");
+// 
+//     const char* msg = "{\"method\":\"getSystemConfig\",\"params\":{}}";
+// 
+//     sendto(sock, msg, strlen(msg), 0, (sockaddr*)&addr, sizeof(addr));
+// 
+//     char buf[1024];
+//     sockaddr_in sender{};
+//     socklen_t senderLen = sizeof(sender);
+// 
+//     std::cout << "Waiting for response\n";
+// 
+//     while (true) {
+//         int len = recvfrom(sock, buf, sizeof(buf) - 1, 0, (sockaddr*)&sender, &senderLen);
+//         if (len > 0) { 
+//             buf[len] = '\0';
+// 
+//             std::cout << "Found bulb at "
+//                       << inet_ntoa(sender.sin_addr)
+//                       << "\nResponse: "
+//                       << buf << "\n\n";
+//         }
+// 
+//     }
+// 
+//     close(sock);
+// }
 
 int main(int argc, char** argv)
 {
@@ -18,7 +62,7 @@ int main(int argc, char** argv)
     // program
     Bulb bulbs[BULB_COUNT];
 
-    bulbs[0].SetDeviceIP("192.168.1.33");
+    bulbs[0].SetDeviceIP("192.168.1.24");
     // bulbs[1].SetDeviceIP("192.168.1.34");
     // bulbs[2].SetDeviceIP("192.168.1.35");
     // bulbs[3].SetDeviceIP("192.168.1.36");
